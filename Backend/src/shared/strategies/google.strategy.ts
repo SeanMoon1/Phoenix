474898efcd940d39a1da090c873769d72209a9f5
SeungYeon Moon ( Sean ) <moonaki43@gmail.com>
2025-09-18 10:44:14 +0900

    OAuth 설정 오류 해결

--- Backend/src/shared/strategies/google.strategy.ts
+++ Backend/src/shared/strategies/google.strategy.ts
@@ -20,12 +20,13 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     profile: any,
     done: VerifyCallback,
   ): Promise<any> {
-    const { name, emails, photos } = profile;
+    const { name, emails, photos, id } = profile;
     const user = {
       email: emails[0].value,
-      firstName: name.givenName,
-      lastName: name.familyName,
-      picture: photos[0].value,
+      name: `${name.givenName} ${name.familyName}`.trim(),
+      profileImage: photos[0]?.value,
+      provider: 'google',
+      providerId: id,
       accessToken,
       refreshToken,
     };
